fa057409662c4d5d4f2edd64c22db7c5a7f9d856
MJackson <jackswalter7@gmail.com>
2020-08-23 05:43:09 +0200

    adding admin panel and fixing some issues from blog and article pages

---
+++ UI/js/admin_index.js
@@ -0,0 +1,9 @@
+function openNav() {
+  document.getElementById("sidenav").style.width = "200px";
+  document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
+}
+
+function closeNav() {
+  document.getElementById("sidenav").style.width = "0";
+  document.body.style.backgroundColor = "white";
+}
